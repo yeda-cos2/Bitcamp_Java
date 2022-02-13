59dedb3ba131c097536e595d147974d872e4e9fc
yeda02 <moon.oyds2@gmail.com>
2022-02-13 23:30:43 +0900

    20220213

--- work/Example1.java
+++ work/Example1.java
@@ -1,62 +1,37 @@
 public class Example1 {
-    public static void main(String args[]) {
-        
-        int i = Integer.parseInt(args[0]);
+	public static void main(String args[]) {
+
+		int i = Integer.parseInt(args[0]);
 		// 1. 입력값이 0개일 경우 받지 않기
 		// if(args.length ==0){
-		//		System.out.println("0~100사이값 입력 요망: ");
-		//		return;
-		//	}
+		// System.out.println("0~100사이값 입력 요망: ");
+		// return;
+		// }
 
 		// 2. 입력값이 1개가 아닐 경우 전부 다 받지 않기
-		if(args.length != 1){
+		if (args.length != 1) {
 			System.out.println("0~100사이값 입력 요망: ");
 			return;
 		}
 
-		if(i>100 || i<0){
-			System.out.println("0~100사이값 입력 요망: ");
-			return;
+		String a;
+
+		if (i >= 0 && i <= 100) {
+			if (i >= 90) {
+				a = "수";
+			} else if (i >= 80) {
+				a = "우";
+			} else if (i >= 70) {
+				a = "미";
+			} else if (i >= 60) {
+				a = "양";
+			} else {
+				a = "가";
+			}
+			System.out.println("입력하신 값 " + i + "점은 " + a + " 입니다.");
+
+		} else {
+			System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
 		}
-
-		String grade="수";
-		
-		switch(i/10){
-			case 10:
-			case 9:
-				break;
-			case 8:
-				grade="우";
-			case 7:
-				grade="미";
-			case 6:
-				grade="양";
-			default:
-				grade="가";
-		}
-		System.out.println("입력하신 값 "+i+"점은 "+grade+"입니다.");
-
-
-
-     //   String a; 
-        
-      //  if(i>=0 && i<=100){
-      //      if(i>=90){
-      //          a="수";
-      //      }else if(i>=80){
-      //          a="우";
-      //      }else if(i>=70){
-      //          a="미";
-      //      }else if(i>=60){
-      //          a="양";
-      //      }else{
-      //          a="가";
-      //      }
-      //      System.out.println("입력하신 값 "+i+"점은 "+a+" 입니다.");
-      
-	  
-	  // }else{
-      //     System.out.println("잘못 입력하셨습니다. 다시 입력해주세요");
-      //  } //end of if
-    } //end of main
-} // end of class
+	}
+}
--- work/Example5.java
+++ work/Example5.java
@@ -1,16 +1,15 @@
-public class Example5 
-{
-	public static void main(String[] args) 
-	{
-		int[][] text=new int[9][9];
+public class Example5 {
+	public static void main(String[] args) {
+		int[][] text = new int[9][9];
 
-		for(int i=0;i<9;i++){
-			System.out.println((i+1)+"´ÜÀ» Ãâ·ÂÇÕ´Ï´Ù.");
+		for (int i = 0; i < 9; i++) {
+			System.out.println((i + 1) + "ë‹¨ì„ ì¶œë ¥í•©ë‹ˆë‹¤.");
 
-			for(int k=0;k<9;k++){
-				System.out.println("text["+i+"]["+k+"]:"+(i+1)+"*"+(k+1)+"="+(i+1)*(k+1));
-	}
+			for (int k = 0; k < 9; k++) {
+				text[i][k] = (i + 1) + "*" + (k + 1) + "=" + (i + 1) * (k + 1);
+				System.out.println("text[" + i + "][" + k + "]:" + text[i][k]);
+			}
 
-}
+		}
 	}
 }
