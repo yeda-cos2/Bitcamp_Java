f779f06e65849feb3b91eac46a5a6a99fc7d4363
yeda02 <moon.oyds2@gmail.com>
2022-02-15 22:38:02 +0900

    20220215

--- work/Prob2.java
+++ work/Prob2.java
@@ -1,21 +1,21 @@
-public class Prob2{
+public class Prob2 {
 
-	public static void main(String[] args){
+	public static void main(String[] args) {
 
-		int limit=50;
+		int limit = 50;
 		boolean prime = false;
 
-		for(int i=2; i<=limit; i++){
-			for(int k=2; k<i; k++){
-				prime = (i%k == 0);
-				if(prime){
+		for (int i = 2; i <= limit; i++) {
+			for (int k = 2; k < i; k++) {
+				prime = (i % k == 0);
+				if (prime) {
 					break;
 				}
 			}
-			if(!prime){
-			System.out.println(i);
-		}
+			if (!prime) {
+				System.out.println(i);
+			}
 		}
 
 	}
-	}
+}
